0611a6b2e35eadbb2604ef614168d923f8f4dc39
Ogungbemi Jeremiah Kola <107107380+Jerriward007@users.noreply.github.com>
2025-06-04 10:28:21 +0100

    Add main driver script to run visualization functions

---
+++ main.py
@@ -0,0 +1,9 @@
+# This file is only for local testing and development.
+# It will not be used for grading.
+
+import time_series_visualizer
+
+# Run the visualizer functions to generate and save the plots
+time_series_visualizer.draw_line_plot()
+time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_box_plot()
